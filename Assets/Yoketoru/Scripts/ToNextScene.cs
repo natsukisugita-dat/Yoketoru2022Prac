38b3f3543f417d128ed831730bce0917cdf69aa0
A601-11 <tnk.dat.class@gmail.com>
2022-10-06 15:09:47 +0900

    ゲーム開始
    TinyAudioとTitleManagerを追加

---
+++ Assets/Yoketoru/Scripts/ToNextScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+// 継承 = 他のクラスの機能を受け継いで拡張
+public class ToNextScene : MonoBehaviour
+{
+    [Tooltip("切り替えたいシーン名"), SerializeField]
+    string nextScene = default;
+
+    //何回も呼び出されないようにするため
+    bool sceneChanged;
+
+    public void ChangeScene()
+    {
+        if (sceneChanged) return;
+
+        sceneChanged = true;
+        TinyAudio.PlaySE(TinyAudio.SE.Start);
+        SceneManager.LoadScene(nextScene);
+    }
+}
