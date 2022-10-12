1683d6028992e83ddbf5e041180e805c7764b810
A601-11 <tnk.dat.class@gmail.com>
2022-10-12 12:00:31 +0900

    シーン切り替えとBGM再生

---
+++ Assets/Yoketoru/Scripts/BGMChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMChanger : MonoBehaviour
+{
+    [Tooltip("鳴らすBGM"), SerializeField]
+    TinyAudio.BGM bgm = TinyAudio.BGM.GameOver;
+
+    void Start()
+    {
+        TinyAudio.PlayBGM(bgm);
+    }
+}
